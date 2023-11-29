b26b774946824417c058420975ad6964c8c9c5f9
ahmed <ahziani@sqli.com>
2023-11-29 13:17:27 +0100

    init

--- src/app/shared/validators/number-validators.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl, ValidatorFn } from "@angular/forms";
-
-export function numberValidators(): ValidatorFn {
-    return (control: AbstractControl): {[key: string]: any} | null => {
-        const isNotNumber = isNaN(control.value);
-        return isNotNumber? { 'notNumber': {value: control.value} }: null
-    }
-}
